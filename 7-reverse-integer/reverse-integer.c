1e0794ca22e09e4ed2bcc6f57a4a14d6f8837c1c
suyash1517 <159546461+suyash1517@users.noreply.github.com>
2025-07-10 22:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (33.53%) - LeetSync

--- 7-reverse-integer/reverse-integer.c
+++ 7-reverse-integer/reverse-integer.c
@@ -1,14 +1,13 @@
 int reverse(int x){
-    long int rev=0;
+    long int rev=0; 
     long int rem;
-    
     while(x!=0)
     {
         rem= x%10;
         rev = rev*10 + rem;
         x=x/10;      
     }
-    if(rev>INT32_MAX || rev<INT32_MIN)
+    if(rev>INT32_MAX || rev<INT32_MIN) // 32bit integer range from -2^31 to 2^31-1
     {
         return 0;
     }
